657a9522b5329ab07d038064de93fa98b3bf7400
thomasgsales <tgaabriel444@gmail.com>
2024-08-30 19:34:23 -0300

    :root

--- landing-page-sandro-vite/src/main.tsx
+++ landing-page-sandro-vite/src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Home from './pages/home.tsx'
+import Header from './pages/header.tsx'
 import './styles/index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Header />
     <Home />
   </StrictMode>
 )
---
+++ landing-page-sandro-vite/src/pages/header.tsx
@@ -0,0 +1,72 @@
+import myLogo from "../assets/logo/logo.svg";
+import "../styles/header.css";
+import "../styles/utility.css";
+import { useState } from 'react';
+import Button from "../components/Button"
+import Close from "../assets/Close.svg"
+import Menu from "../assets/Menu.svg"
+import "../styles/buttons.css"
+
+export default function Header() {
+    const[showMobileMenu, setShowMobileMenu] = useState(false);
+    return (
+        <>
+           <header className='container py-sm'>
+                <nav className='flex items-center justify-between'>
+                    <img src={myLogo} alt="logo" width={200} height={60}/>
+                
+                    <div className='desktop-only'>
+                        <ul className="flex gap-1">
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Soluções</a></li>
+                            <li><a href="#">Depoimento</a></li>
+                            <li><a href="#">Preços</a></li>
+                            <li><a href="#">Contato</a></li>
+                        </ul>
+                    </div>
+                    <div className='desktop-only'>
+                        <div className='flex items-center'>
+                            <a className='reverse-color ml-lg' href="">Login</a>
+                            <Button text="cadastre-se" />
+                        </div>
+                    </div>
+                    <div className='mobile-menu'>
+                        {showMobileMenu ?
+                            <div className='mobile-menu-content'>
+                                 <div className="container flex">
+                                    <ul>
+                                        <li>
+                                            <a href="#">Home</a>
+                                        </li>
+                                        <li>
+                                            <a href="#solution">Soluções</a>
+                                        </li>
+                                        <li>
+                                            <a href="#testimonials">Depoimentos</a>
+                                        </li>
+                                        <li>
+                                            <a href="#pricing">Preços</a>
+                                        </li>
+                                        <li>
+                                            <a href="#contact">Contato</a>
+                                        </li>
+                                        <li>
+                                            <a className="reverse-color" href="#">Login</a>
+                                        </li>
+                                    </ul>
+                                    <span onClick={() => setShowMobileMenu(!showMobileMenu)} className='btn-wrapper'>
+                                        <img src={Close} alt="ícone fechar menu" width={24} height={24} />
+                                    </span>
+                                </div>
+                            </div>
+                            :
+                            <span onClick={() => setShowMobileMenu(!showMobileMenu)} className='btn-wrapper'>
+                                <img src={Menu} alt="ícone menu" width={24} height={24} />
+                            </span>
+                        }
+                    </div>
+                </nav>
+            </header>
+        </>
+    )
+}
