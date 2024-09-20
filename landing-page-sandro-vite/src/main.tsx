2ee209ea487c2a9d07c6baa7f27e2d013a8f6db9
thomasgsales <tgaabriel444@gmail.com>
2024-09-20 20:24:24 -0300

    main 1.0

--- landing-page-sandro-vite/src/main.tsx
+++ landing-page-sandro-vite/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Home from './pages/home.tsx'
-import Header from './pages/header.tsx'
+import Header from './components/header.tsx'
 import './styles/index.css'
 
 createRoot(document.getElementById('root')!).render(
